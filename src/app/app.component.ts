ea9c35ce62c2c4bc234ac6d21fb365f34e743df0
Kowsik Petluri <Kowsik.Petluri@lntinfotech.com>
2023-07-03 15:44:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup , Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-form';
+  constructor( private fb: FormBuilder){
+
+   }
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('kowshik'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city : new FormControl(''),
+  //     state : new FormControl(''),
+  //     postalCode : new FormControl(''),
+  //       })
+  // });
+  // loadApiData(){
+  //   this.registrationForm.patchValue({
+  //     userName :'kowshik',
+  //     password : '1234',
+  //     confirmPassword:'1234'
+  //   });
+
+  // }
+  registrationForm = this.fb.group({
+    userName: ["kowshik", Validators.required],
+    password:[""],
+    confirmPassword:[""],
+    address: this.fb.group({
+      city: [''],
+    state:[""],
+    postalCode:['']
+    })  })
+
 }
